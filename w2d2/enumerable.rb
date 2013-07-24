4179a02b6b9b0d1c9f900896a8172acb89f0d68e
App Academy Student <student@appacademy.io>
2013-07-23 23:52:09 -0700

    Add inject method; unsure how to mixin

--- w2d2/enumerable.rb
+++ w2d2/enumerable.rb
@@ -1,7 +1,30 @@
 module MyEnumerable
   # Assuming Array at this time
 
-  def inject(&prc)
+  def inject(*arg, &prc)
+    if arg.size == 0
+      return nil if prc.nil? # Actually, throw exception?
+
+      memo = self[0]
+      self[1..-1].each do |element|
+        memo = prc.call(memo, element)
+      end
+      return memo
+    elsif arg.size == 1
+      if arg[0].is_a?(Symbol)
+        # TODO; remember to don't care about the proc
+      else
+        return nil if prc.nil? # Actually, throw exception?
+        memo = arg[0]
+        self.each do |element|
+          memo = prc.call(memo, element)
+        end
+      end
+    elsif arg.size == 2
+      # TODO
+    else
+      return nil # Actually, throw exception?
+    end
   end
 
   def map(&prc)

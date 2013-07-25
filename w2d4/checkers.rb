6c1300c4010668cdcb9e7e60bf9ab392e208d240
App Academy Student <student@appacademy.io>
2013-07-25 15:43:47 -0700

    valid_move? works; also organized helper methods

--- w2d4/checkers.rb
+++ w2d4/checkers.rb
@@ -1,7 +1,7 @@
-require '../w2d2/chess'
+require_relative '../w2d2/chess'
 
-class Checkers
-  attr_reader :board
+class Checkers < Game
+  attr_reader :board, :white, :red
 
   def initialize
     @board = CheckerBoard.new
@@ -11,32 +11,33 @@ class Checkers
     @board.place_pieces
 
     @white = HumanPlayer.new(self, :white)
-    @red = HumanPlayer.new(self, :red)
+    @red   = HumanPlayer.new(self, :red)
+
+    player_toggle = true
+    while false # While the game is still on
+      if player_toggle
+        move_pos = @white.make_move
+
+        @board.move_pieces(move_pos)
+        player_toggle = false
+      else
+        move_pos = @red.make_move
+
+        @board.move_pieces(move_pos)
+        player_toggle = true
+      end
+    end
   end
 end
 
 class CheckerBoard < Board
   def initialize
-    super
+    super # Creates initial @board instance
     @board = create_checker_board(@board)
   end
 
-  def create_checker_board(board)
-    BOARD_SIZE.times do |row|
-      BOARD_SIZE.times do |col|
-        row_col_equal_parity = row.even? == col.even?
+  def move_pieces(move_pos)
 
-        board[row][col] = :forbidden_tile if row_col_equal_parity
-      end
-    end
-    board
-  end
-
-  def place_checker_piece(row, side)
-    BOARD_SIZE.times do |col|
-      @board[row][col] = CheckerPiece.new([row, col], side) \
-                          unless @board[row][col] == :forbidden_tile
-    end
   end
 
   def place_pieces
@@ -49,6 +50,29 @@ class CheckerBoard < Board
     (5..7).each do |row|
       place_checker_piece(row, :white)
     end
+  end
+
+  def valid_move?(move_pos, side)
+    current_pos, next_pos = move_pos
+
+    return false unless piece_exist?(current_pos)
+    return false unless side == @board[current_pos[0]][current_pos[1]].side
+
+    # Make sure all moves are diagonal & correct move length
+    row_diff_abs = (current_pos[0] - next_pos[0]).abs
+    col_diff_abs = (current_pos[1] - next_pos[1]).abs
+    return false unless row_diff_abs == col_diff_abs
+    return false if row_diff_abs > 2 # && col_diff_abs > 2
+
+    return false if piece_exist?(next_pos)
+
+    if row_diff_abs == 2
+      return false unless valid_jump?(move_pos, side)
+    else # if row_diff == 1
+      return false unless valid_slide?(move_pos, side)
+    end
+
+    true
   end
 
   def to_s
@@ -73,9 +97,62 @@ class CheckerBoard < Board
     puts '  ' + '                 '.bg_cyan
     puts '   a b c d e f g h'
   end
+
+  # Helper methods
+  def create_checker_board(board)
+    BOARD_SIZE.times do |row|
+      BOARD_SIZE.times do |col|
+        row_col_equal_parity = row.even? == col.even?
+
+        board[row][col] = :forbidden_tile if row_col_equal_parity
+      end
+    end
+    board
+  end
+
+  def place_checker_piece(row, side)
+    BOARD_SIZE.times do |col|
+      @board[row][col] = CheckerPiece.new([row, col], side) \
+                          unless @board[row][col] == :forbidden_tile
+    end
+  end
+
+  def valid_jump?(move_pos, side)
+    current_pos, next_pos = move_pos
+
+    target_row = (current_pos[0] + next_pos[0]) / 2
+    target_col = (current_pos[1] + next_pos[1]) / 2
+
+    return false if @board[target_row][target_col].nil?
+
+    sides = [:white, :red]
+    sides.delete(side)
+    enemy_side = sides[0]
+
+    return false unless @board[target_row][target_col].side == enemy_side
+    true
+  end
+
+  def valid_slide?(move_pos, side)
+    current_pos, next_pos = move_pos
+
+    return true if @board[current_pos[0]][current_pos[1]].king
+
+    row_diff = current_pos[0] - next_pos[0]
+    # Side-specific checks
+    if side == :white
+      return false unless row_diff > 0 # Always going forward
+    else # side == :red
+      return false unless row_diff < 0
+    end
+
+    true
+  end
 end
 
 class CheckerPiece < Piece
+  attr_reader :king
+
   def initialize(pos, side)
     super(pos, side)
     @king = false
@@ -89,4 +166,10 @@ end
 class String
   def red;            "\033[31m#{self}\033[0m" end
   def bg_blue;        "\033[44m#{self}\033[0m" end
+end
+
+def debug
+  c = Checkers.new
+  c.play
+  c.red.make_move
 end

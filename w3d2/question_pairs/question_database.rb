ac7b9a2a284ec3bab0890ec87ade2419ca70fabf
Ed Solis <edsfocci@gmail.com>
2013-08-04 00:42:49 -0700

    Add class files

---
+++ w3d2/question_pairs/question_database.rb
@@ -0,0 +1,12 @@
+require 'singleton'
+require 'sqlite3'
+
+class QuestionDatabase < SQLite3::Database
+	include Singleton
+
+	def initialize
+		super(questions.db)
+		self.results_as_hash = true
+		self.type_translation = true
+	end
+end

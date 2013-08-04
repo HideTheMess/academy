2ebb3cc291f1cb744e001361b7e871ab7f48e6ce
Ed Solis <edsfocci@gmail.com>
2013-08-04 16:49:01 -0700

    Fix tabs for question_pairs

--- w3d2/question_pairs/import_db.sql
+++ w3d2/question_pairs/import_db.sql
@@ -1,72 +1,72 @@
 CREATE TABLE users
 (
-	id INTEGER PRIMARY KEY,
-	fname VARCHAR(255) NOT NULL,
-	lname VARCHAR(255) NOT NULL
+  id INTEGER PRIMARY KEY,
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL
 );
 
 INSERT INTO
-	users (fname, lname)
+  users (fname, lname)
 VALUES
-	('Albert', 'Einstein'), ('Kurt', 'Godel'), ('Babe', 'Ruth'),
-	('Freddie', 'Wong');
+  ('Albert', 'Einstein'), ('Kurt', 'Godel'), ('Babe', 'Ruth'),
+  ('Freddie', 'Wong');
 
 CREATE TABLE questions
 (
-	id INTEGER PRIMARY KEY,
-	title VARCHAR(255) NOT NULL,
-	body TEXT NOT NULL,
-	author_id INTEGER NOT NULL,
-	FOREIGN KEY (author_id) REFERENCES users(id)
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
 INSERT INTO
-	questions (title, body, author_id)
+  questions (title, body, author_id)
 VALUES
-	('Albie', 'Yoyoyo', 1);
+  ('Albie', 'Yoyoyo', 1);
 
 CREATE TABLE question_followers
 (
-	id INTEGER PRIMARY KEY,
-	user_id INTEGER NOT NULL,
-	question_id INTEGER NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES users(id),
-	FOREIGN KEY (question_id) REFERENCES questions(id)
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 INSERT INTO
-	question_followers (user_id, question_id)
+  question_followers (user_id, question_id)
 VALUES
-	(2, 1);
+  (2, 1);
 
 CREATE TABLE replies
 (
-	id INTEGER PRIMARY KEY,
-	reply TEXT NOT NULL,
-	question_id INTEGER NOT NULL,
-	parent_id INTEGER,
-	user_id INTEGER NOT NULL,
-	FOREIGN KEY (question_id) REFERENCES questions(id),
-	FOREIGN KEY (parent_id) REFERENCES replies(id),
-	FOREIGN KEY (user_id) REFERENCES users(id)
+  id INTEGER PRIMARY KEY,
+  reply TEXT NOT NULL,
+  question_id INTEGER NOT NULL,
+  parent_id INTEGER,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 INSERT INTO
-	replies (reply, question_id, parent_id, user_id)
+  replies (reply, question_id, parent_id, user_id)
 VALUES
-	('Not Cool', 1, NULL, 3),
-	('Naw, very cool', 1, 1, 4);
+  ('Not Cool', 1, NULL, 3),
+  ('Naw, very cool', 1, 1, 4);
 
 CREATE TABLE question_likes
 (
-	id INTEGER PRIMARY KEY,
-	user_id INTEGER NOT NULL,
-	question_id INTEGER NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES users(id),
-	FOREIGN KEY (question_id) REFERENCES questions(id)
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 INSERT INTO
-	question_likes (user_id, question_id)
+  question_likes (user_id, question_id)
 VALUES
-	(4, 1);
+  (4, 1);
--- w3d2/question_pairs/question_database.rb
+++ w3d2/question_pairs/question_database.rb
@@ -4,9 +4,9 @@ require 'sqlite3'
 class QuestionDatabase < SQLite3::Database
   include Singleton
 
-	def initialize
-		super('questions.db')
-		self.results_as_hash = true
-		self.type_translation = true
-	end
+  def initialize
+    super('questions.db')
+    self.results_as_hash = true
+    self.type_translation = true
+  end
 end

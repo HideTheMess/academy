819bf96329fd26ee0d459db28830d319f7b57f44
App Academy Student <student@appacademy.io>
2013-08-05 17:58:32 -0700

    Convert associable in prep for has many through

--- w3d5/active_record_lite/lib/active_record_lite/associatable.rb
+++ w3d5/active_record_lite/lib/active_record_lite/associatable.rb
@@ -3,6 +3,8 @@ require 'active_support/inflector'
 require_relative './db_connection.rb'
 
 class AssocParams
+  attr_reader :primary_key, :foreign_key
+
   def other_class
   end
 
@@ -12,6 +14,32 @@ end
 
 class BelongsToAssocParams < AssocParams
   def initialize(name, params)
+    unless params[:class_name].nil?
+      @other_class_name = params[:class_name]
+    else
+      @other_class_name = name.to_s.capitalize
+      @other_class_name.gsub!(/_[a-z]/) { |w| w[1].upcase }
+    end
+
+    unless params[:primary_key].nil?
+      @primary_key = params[:primary_key]
+    else
+      @primary_key = :id
+    end
+
+    unless params[:foreign_key].nil?
+      @foreign_key = params[:foreign_key]
+    else
+      @foreign_key = "#{ name }_id".to_sym
+    end
+  end
+
+  def other_class
+    @other_class_name.constantize
+  end
+
+  def other_table
+    other_class.table_name
   end
 
   def type
@@ -20,6 +48,34 @@ end
 
 class HasManyAssocParams < AssocParams
   def initialize(name, params, self_class)
+    unless params[:class_name].nil?
+      @other_class_name = params[:class_name]
+      # TODO: Convert @other_class_name to snake case
+    else
+      other_class_name_snake = name.to_s.singularize
+      @other_class_name = other_class_name_snake.capitalize
+      @other_class_name.gsub!(/_[a-z]/) { |w| w[1].upcase }
+    end
+
+    unless params[:primary_key].nil?
+      @primary_key = params[:primary_key]
+    else
+      @primary_key = :id
+    end
+
+    unless params[:foreign_key].nil?
+      @foreign_key = params[:foreign_key]
+    else
+      @foreign_key = "#{ other_class_name_snake }_id".to_sym
+    end
+  end
+
+  def other_class
+    @other_class_name.constantize
+  end
+
+  def other_table
+    other_class.table_name
   end
 
   def type
@@ -31,71 +87,32 @@ module Associatable
   end
 
   def belongs_to(name, params = {})
+    aps = BelongsToAssocParams.new(name, params)
+
     define_method(name) do
-      unless params[:class_name].nil?
-        other_class_name = params[:class_name]
-      else
-        other_class_name = name.to_s.capitalize
-        other_class_name.gsub!(/_[a-z]/) { |w| w[1].upcase }
-      end
 
-      unless params[:primary_key].nil?
-        primary_key = params[:primary_key]
-      else
-        primary_key = :id
-      end
-
-      unless params[:foreign_key].nil?
-        foreign_key = params[:foreign_key]
-      else
-        foreign_key = "#{ name }_id".to_sym
-      end
-
-      other_class = other_class_name.constantize
-      other_table_name = other_class.table_name
-
-      other_record = DBConnection.execute(<<-SQL, send(foreign_key))
+      other_record = DBConnection.execute(<<-SQL, send(aps.foreign_key))
         SELECT *
-        FROM #{ other_table_name }
+        FROM #{ aps.other_table }
         WHERE id = ?
       SQL
 
-      other_class.parse_all(other_record)
+      aps.other_class.parse_all(other_record)
     end
   end
 
   def has_many(name, params = {})
+    aps = HasManyAssocParams.new(name, params, self.class)
+
     define_method(name) do
-      unless params[:class_name].nil?
-        other_class_name = params[:class_name]
-      else
-        other_class_name_snake = name.to_s.singularize
-        other_class_name = other_class_name_snake.capitalize
-        other_class_name.gsub!(/_[a-z]/) { |w| w[1].upcase }
-      end
 
-      unless params[:primary_key].nil?
-        primary_key = params[:primary_key]
-      else
-        primary_key = :id
-      end
-
-      unless params[:foreign_key].nil?
-        foreign_key = params[:foreign_key]
-      else
-        foreign_key = "#{ other_class_name_snake }_id".to_sym
-      end
-
-      other_class = other_class_name.constantize
-      other_table_name = other_class.table_name
-
-      other_records = DBConnection.execute(<<-SQL, send(primary_key))
+      other_records = DBConnection.execute(<<-SQL, send(aps.primary_key))
         SELECT *
-        FROM #{ other_table_name }
+        FROM #{ aps.other_table }
         WHERE owner_id = ?
       SQL
 
-      other_class.parse_all(other_records)
+      aps.other_class.parse_all(other_records)
     end
   end
 

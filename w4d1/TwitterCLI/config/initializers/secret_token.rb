7c43c57a9642710fd0c50975f1e80c39035c4068
William Li and Korey Kassir <student@appacademy.io>
2013-08-06 22:34:20 -0700

    Create TwitterCLI

---
+++ w4d1/TwitterCLI/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterCLI::Application.config.secret_token = 'e0961594bb9b5e1d4f6885fb463c88ac88c2988c457bb8b4ec58618aef67e574d79ea92314111ea5fbc3ae7e6c5e00e2877bac3be6687d018bae838de153709b'
